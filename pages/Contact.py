35c867f6aa6ef6884c9d290bdf7f643678c5d829
Ayushi0628 <rathodayushi74@gmail.com>
2026-04-21 21:13:00 -0400

    V9

--- pages/Contact.py
+++ pages/Contact.py
@@ -96,7 +96,7 @@ with col_right:
 
     st.markdown('<div class="sh" style="margin-top:1.5rem;">Connect <span>Online</span></div>', unsafe_allow_html=True)
     st.link_button("🔗 LinkedIn Profile", "https://linkedin.com/in/ayushirathod", use_container_width=True)
-    st.link_button("🐙 GitHub Profile",   "https://github.com/ayushirathod",     use_container_width=True)
+    st.link_button("🐙 GitHub Profile",   "https://github.com/Ayushi0628",     use_container_width=True)
 
 st.markdown('<hr class="divider"/>', unsafe_allow_html=True)
 
